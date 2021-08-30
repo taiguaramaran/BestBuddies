26125dc12686fb67c81315985c3aaa7a41382ce5
Lucas Martelli <lucas98martelli@hotmail.com>
2021-08-30 17:37:32 -0300

    controller and routes done

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,2 +1,50 @@
 class PartnersController < ApplicationController
+  def index
+    @partners = Partner.all
+  end
+
+  def show
+    @partner = Partner.find(params[:id])
+  end
+
+  def new
+    @partner = Partner.new
+  end
+
+  def create
+    @partner = Partner.new(partner_params)
+
+    if @partner.save
+      redirect_to partner_path(@partner)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @partner = Partner.find(params[:id])
+  end
+
+  def update
+    @partner = Partner.find(params[:id])
+
+    if @partner.update(partner_params)
+      redirect_to partner_path(@partner)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @partner = Partner.find(params[:id])
+    @partner.destroy
+
+    redirect_to partners_path
+  end
+
+  private
+
+  def partner_params
+    params.require(:partners).permit(:name, :address)
+  end
 end
