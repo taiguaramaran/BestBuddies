92741c8093f8a00deeacd87eed7a91e34aec4c48
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-08-31 20:09:57 +0200

    pundit installed

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,7 @@
 class Animal < ApplicationRecord
   CATEGORIES = ["CAT", "DOG"]
-  AGES = ["Até seis meses", "Até 2 anos", "Até 5 anos", "Mais que 5 anos"]   
+  AGES = ["Até seis meses", "Até 2 anos", "Até 5 anos", "Mais que 5 anos"]
+
   has_many_attached :photos
   belongs_to :user
   belongs_to :partner, optional: true
