ac7cee9eca89c13698e6312bc12aaef97fd7491d
taiguara silva <taiguaramaran@gmail.com>
2021-08-31 13:46:47 -0300

    animal controller with validations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,10 @@
 class Animal < ApplicationRecord
+  CATEGORIES = ["CAT", "DOG"]
+  AGES = ["Até seis meses", "Até 2 anos", "Até 5 anos", "Mais que 5 anos"]   
   has_many_attached :photos
   belongs_to :user
   belongs_to :partner, optional: true
+
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :age, inclusion: { in: AGES}
 end
