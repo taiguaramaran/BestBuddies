510f482d06bcd72b89fd313005be37444e9fd189
taiguara silva <taiguaramaran@gmail.com>
2021-08-31 15:59:39 -0300

    pundit

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,16 +1,19 @@
 class AnimalsController < ApplicationController
 
   def index
-    @animals = Animal.all
+    @animals = policy_scope(Animal)
   end
 
   def new
     @animal = Animal.new
+    authorize @animal
   end
 
   def create
+    
     @animal = Animal.new(animal_params)
     @animal.user = current_user
+    authorize @animal
     if @animal.save
       redirect_to animals_path
     else
@@ -20,14 +23,17 @@ class AnimalsController < ApplicationController
 
   def show
     @animal = Animal.find(params[:id])
+    authorize @animal
   end
 
   def edit
     @animal = Animal.find(params[:id])
+    authorize @animal
   end
 
   def update
     @animal = Animal.find(params[:id])
+    authorize @animal
     if @animal.update(animal_params)
       redirect_to animal_path(@animal)
     else
@@ -38,6 +44,7 @@ class AnimalsController < ApplicationController
   def destroy
     @animal = Animal.find(params[:id])
     @animal.destroy
+    authorize @animal
     redirect_to animals_path
   end
 
