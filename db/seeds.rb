e078bded17add432d7c7d87fbd8896a47d16389d
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-09-02 21:47:20 +0200

    animals seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,58 @@
 require "open-uri"
 
-User.destroy_all
 Animal.destroy_all
+User.destroy_all
 
 ###################USERS##################
-# puts "Creating Users ..."
-# User.create(name: 'taiguara', address: 'rua 1', age: '32', email:'taiguaramaran@gmail.com', password:'123123')
-# puts "Users Created !!!"
+puts "Creating Users ..."
+User.create(name: 'Taiguara', address: 'rua 1', age: '32', email:'taiguaramaran@gmail.com', password:'123123')
+puts "Users Created !!!"
 
 
 ###################ANIMALS##################
 
 puts 'Creating Animals ...'
 
-file = URI.open('https://dog.ceo/api/breeds/image/random')
 
-toto = Animal.create(user: User.first, name: 'Toto', breed: 'mixed', category: "dog", size: "Small", gender: "Female", age: "Up to six months old")
+image_url = JSON.parse(URI.open('https://dog.ceo/api/breeds/image/random').read)['message']
+file = URI.open(image_url)
+toto = Animal.new(user: User.first, name: 'Toto', breed: 'mixed', category: "dog", size: "Small", gender: "Female", age: "Up to six months old")
 toto.photos.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+toto.save!
 
-duke = Animal.create(user: User.first, name: 'Duke', breed: 'German Shepherd Dog', category: "dog", size: "Big", gender: "Male", age: "More than 5 years old")
+image_url = JSON.parse(URI.open('https://dog.ceo/api/breeds/image/random').read)['message']
+file = URI.open(image_url)
+duke = Animal.new(user: User.first, name: 'Duke', breed: 'German Shepherd Dog', category: "dog", size: "Big", gender: "Male", age: "More than 5 years old")
 duke.photos.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+duke.save!
 
-lacey = Animal.create(user: User.first, name: 'Lacey', breed: 'Chihuahua/French Bulldog Mix', category: "dog", size: "Small", gender: "Female", age: "Up to six months old")
+image_url = JSON.parse(URI.open('https://dog.ceo/api/breeds/image/random').read)['message']
+file = URI.open(image_url)
+lacey = Animal.new(user: User.first, name: 'Lacey', breed: 'Chihuahua/French Bulldog Mix', category: "dog", size: "Small", gender: "Female", age: "Up to six months old")
 lacey.photos.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+lacey.save!
 
-bradley = Animal.create(user: User.first, name: 'Bradley', breed: 'Chihuahua Mix', category: "dog", size: "Small", gender: "Male", age: "Up to six months old")
+image_url = JSON.parse(URI.open('https://dog.ceo/api/breeds/image/random').read)['message']
+file = URI.open(image_url)
+bradley = Animal.new(user: User.first, name: 'Bradley', breed: 'Chihuahua Mix', category: "dog", size: "Small", gender: "Male", age: "Up to six months old")
 bradley.photos.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+bradley.save!
 
-bradley = Animal.create(user: User.first, name: 'Lobo', breed: 'German Shepherd Dog', category: "dog", size: "Big", gender: "Male", age: "More than 5 years old")
-bradley.photos.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+image_url = JSON.parse(URI.open('https://dog.ceo/api/breeds/image/random').read)['message']
+file = URI.open(image_url)
+lobo = Animal.new(user: User.first, name: 'Lobo', breed: 'German Shepherd Dog', category: "dog", size: "Big", gender: "Male", age: "More than 5 years old")
+lobo.photos.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+lobo.save!
 
 file = URI.open('https://cataas.com/cat/cute')
-
-figaro = Animal.create(user: User.first, name: 'Figaro', breed: 'Domestic', category: "cat", size: "Small", gender: "Male", age: "More than 5 years old")
+figaro = Animal.new(user: User.first, name: 'Figaro', breed: 'Domestic', category: "cat", size: "Small", gender: "Male", age: "More than 5 years old")
 figaro.photos.attach(io: file, filename: 'cat.png', content_type: 'image/png')
+figaro.save!
 
-gracie = Animal.create(user: User.first, name: 'Gracie', breed: 'Domestic', category: "cat", size: "Small", gender: "Male", age: "Up to 2 years old")
+file = URI.open('https://cataas.com/cat/cute')
+gracie = Animal.new(user: User.first, name: 'Gracie', breed: 'Domestic', category: "cat", size: "Small", gender: "Male", age: "Up to 2 years old")
 gracie.photos.attach(io: file, filename: 'cat.png', content_type: 'image/png')
+gracie.save!
 
 puts 'Animals created!!!'
 
