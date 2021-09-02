93cad8b2078882d8da801f96f4d83c7475b2215c
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-09-02 21:01:02 +0200

    deleted animals folder from images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
 require "open-uri"
 
-
 User.destroy_all
 Animal.destroy_all
 
 ###################USERS##################
-puts "Creating Users"
-User.create(name: 'taiguara', address: 'rua 1', age: '32', email:'taiguaramaran@gmail.com', password:'123123')
-puts "Users Created"
+# puts "Creating Users"
+# User.create(name: 'taiguara', address: 'rua 1', age: '32', email:'taiguaramaran@gmail.com', password:'123123')
+# puts "Users Created"
 
 
 ###################ANIMALS##################
+
 puts 'Creating Animal'
-Animal.create(user: User.first, name: 'Toto', breed: 'mixed', category: "dog", size: "Small", gender: "Female", age: "Up to six months old")
+toto = Animal.create(user: User.first, name: 'Toto', breed: 'mixed', category: "dog", size: "Small", gender: "Female", age: "Up to six months old")
+toto.photo.attach(io: file)
+
 Animal.create(user: User.first, name: 'Duke', breed: 'German Shepherd Dog', category: "dog", size: "Big", gender: "Male", age: "More than 5 years old")
 puts 'Animal created'
 
 ###################PARTNERS##################
-puts 'Creating Partner'
-Partner.create
-puts 'Partner created'
+# puts 'Creating Partner'
+# Partner.create
+# puts 'Partner created'
